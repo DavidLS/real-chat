d6f21647b23bddfb14d5deba3eefd4c1bf458c2c
David L <davidelischinsky@gmail.com>
2021-05-25 10:22:04 -0300

    format message's time

--- src/Chat/MessageBlock/MessageBlock.js
+++ src/Chat/MessageBlock/MessageBlock.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { format, isToday, isYesterday } from 'date-fns'
 
-const MessageBlock = ({ text, user }) => {
+const getDayName = (date) => {
+  if (isToday(date)) return ''
+  if (isYesterday(date)) return 'Yesterday'
+  return format(date, 'EEE')
+}
+
+const formatTime = (timeRaw) => {
+  const date = new Date(timeRaw)
+  const dayName = getDayName(date)
+
+  const time = format(date, 'h:mm aaa')
+
+  return `${dayName + ' '}${time}`
+}
+
+const MessageBlock = ({ text, time, user }) => {
   const encodedUri = encodeURI(user)
+  const timeString = formatTime(time)
   return (
     <div>
      <img src={`https://ui-avatars.com/api/?name=${encodedUri}`} alt={user}
       />
       <div style={{ whiteSpace: 'pre-line' }}>
-        <strong>{user}</strong>: {text}
+        <strong>{user}</strong> ({timeString}): {text}
       </div>
     </div>
   )

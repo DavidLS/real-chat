da09b1de36df1b36f64eb694b009e3876db2d955
David L <davidelischinsky@gmail.com>
2021-05-26 18:01:45 -0300

    add vertical scroll

--- src/components/chat/List/List.js
+++ src/components/chat/List/List.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import MessageBlock from './MessageBlock/'
 
+import styles from './List.module.css'
+
 const List = ({ messages }) => {
   return (
-    messages.map(
-      (message) => {
-        return (
-            <MessageBlock
-              key={`message_${message.time}`}
-              messageObj={message}
-            />
-        )
-      })
+    <div className={styles.List}>
+      {messages.map(
+        (message) => {
+          return (
+              <MessageBlock
+                key={`message_${message.time}`}
+                messageObj={message}
+              />
+          )
+        })
+      }
+    </div>
   )
 }
 

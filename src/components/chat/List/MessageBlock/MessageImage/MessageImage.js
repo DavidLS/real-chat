c3b5efc8041f5b5561a3818eb333bb15731dfd7d
David L <davidelischinsky@gmail.com>
2021-05-26 09:43:40 -0300

    remove unused props

--- src/components/chat/List/MessageBlock/MessageImage/MessageImage.js
+++ src/components/chat/List/MessageBlock/MessageImage/MessageImage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Spinner from '../../../../utils/Spinner'
 
-const MessageImage = ({ alt, avatar, timeString, url, user }) => {
+const MessageImage = ({ alt, url }) => {
   const [isLoading, setIsLoading] = useState(true)
 
   return (
--- src/components/chat/List/MessageBlock/MessageText/MessageText.js
+++ src/components/chat/List/MessageBlock/MessageText/MessageText.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MessageText = ({ text, timeString, user, avatar }) => {
+const MessageText = ({ text, timeString, user }) => {
   return (
     <div style={{ whiteSpace: 'pre-line' }}>
       <strong>{user}</strong> ({timeString}): {text}

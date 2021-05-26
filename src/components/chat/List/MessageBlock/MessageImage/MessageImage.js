ea5e3c80a9d4920c0fe1e9ad62c23fba9d313327
David L <davidelischinsky@gmail.com>
2021-05-26 14:51:29 -0300

    style image messages

--- src/components/chat/List/MessageBlock/MessageImage/MessageImage.js
+++ src/components/chat/List/MessageBlock/MessageImage/MessageImage.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import Spinner from '../../../../utils/Spinner'
 
+import styles from './MessageImage.module.css'
+
 const MessageImage = ({ alt, url }) => {
   const [isLoading, setIsLoading] = useState(true)
 
   return (
     <>
-      {isLoading && <Spinner/>}
+      {isLoading && <Spinner size={'200px'}/>}
       <img
+        className={styles.MessageImage}
         src={url}
         alt={alt}
         onLoad={() => setIsLoading(false)}
